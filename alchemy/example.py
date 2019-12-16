69411aa97c1d263c7d99981a843163941a18157a
David Kuryakin <dkuryakin@gmail.com>
2019-12-16 04:18:04 +0300

    add example

---
+++ alchemy/example.py
@@ -0,0 +1,21 @@
+import random
+
+from alchemy import Logger
+
+token = '...'
+
+for gid in range(3):
+    for eid in range(5):
+        for mid in range(100):
+            metric = f'metric_{mid}'
+            group = f'group_{gid}'
+            experiment = f'experiment_{eid}'
+
+            logger = Logger(token, experiment, group)
+
+            n = 300
+            x = random.randint(-10, 10)
+            for i in range(n):
+                logger.log_scalar(metric, x)
+                x += random.randint(-1, 1)
+            logger.close()
