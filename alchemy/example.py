4216f86f8d4e6794f6c25225e48d992c2ec0def3
Sergey Kolesnikov <scitator@gmail.com>
2019-12-30 08:29:42 +0300

    Update example.py

--- alchemy/example.py
+++ alchemy/example.py
@@ -4,9 +4,9 @@ from alchemy import Logger
 
 token = "..."
 
-for gid in range(3):
-    for eid in range(5):
-        for mid in range(10):
+for gid in range(1):
+    for eid in range(2):
+        for mid in range(3):
             metric = f"metric_{mid}"
             group = f"group_{gid}"
             experiment = f"experiment_{eid}"
