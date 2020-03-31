d8a038926006b1669eae6618f382398437dd568b
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-03-31 16:53:28 +0300

    update codestyle (#9)

--- example.py
+++ example.py
@@ -11,17 +11,14 @@ for gid in range(1):
     for eid in range(2):
         experiment = f"experiment_{eid}"
         logger = Logger(
-            token=token,
-            experiment=experiment,
-            group=group,
-            project=project,
+            token=token, experiment=experiment, group=group, project=project,
         )
         for mid in range(4):
             metric = f"metric_{mid}"
             # let's sample some random data
             n = 300
             x = random.randint(-10, 10)
-            for i in range(n):
+            for _ in range(n):
                 logger.log_scalar(metric, x)
                 x += random.randint(-1, 1)
         logger.close()
